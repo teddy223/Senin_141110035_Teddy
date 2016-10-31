a73f94210c6fa3312d2ff46290bdb2d8fe305039
141110035-Teddy <teddychien2708@gmail.com>
2016-10-31 17:26:41 +0700

    Commit

---
+++ Senin_141110035_Teddy/Latihan_3_1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.FlatStyle == FlatStyle.Standard)
+            {
+                button1.FlatStyle = FlatStyle.Popup;
+            }
+            else
+            {
+                button1.FlatStyle = FlatStyle.Standard;
+            }
+
+            FontStyle style = richTextBox1.SelectionFont.Style;
+
+            if (button1.FlatStyle == FlatStyle.Popup)
+            {
+                style &= ~FontStyle.Bold;
+            }
+            else
+            {
+                style |= FontStyle.Bold;
+            }
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, style);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.FlatStyle == FlatStyle.Standard)
+            {
+                button2.FlatStyle = FlatStyle.Popup;
+            }
+            else
+            {
+                button2.FlatStyle = FlatStyle.Standard;
+            }
+
+            FontStyle style = richTextBox1.SelectionFont.Style;
+
+            if (button2.FlatStyle == FlatStyle.Popup)
+            {
+                style &= ~FontStyle.Italic;
+            }
+            else
+            {
+                style |= FontStyle.Italic;
+            }
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, style);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.FlatStyle == FlatStyle.Standard)
+            {
+                button3.FlatStyle = FlatStyle.Popup;
+            }
+            else
+            {
+                button3.FlatStyle = FlatStyle.Standard;
+            }
+
+            FontStyle style = richTextBox1.SelectionFont.Style;
+
+            if (button3.FlatStyle == FlatStyle.Popup)
+            {
+                style &= ~FontStyle.Underline;
+            }
+            else
+            {
+                style |= FontStyle.Underline;
+            }
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, style);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
